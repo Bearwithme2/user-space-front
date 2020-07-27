771c90ecbe9d584943d178e6a4274d98bb1cdf92
Jakub Trtik <jakubtrtik2@gmail.com>
2020-07-28 00:41:23 +0200

    api connecton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="hello" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      hello: null
+    };
+  },
+  async mounted() {
+    await this.apiHello();
+  },
+  methods: {
+    async apiHello() {
+      let $this = this;
+      await this.$axios("http://127.0.0.1:8000")
+        .then(function(json) {
+          $this.hello = json.data.hello;
+        })
+        .catch(function(e) {
+          console.error(e.message);
+        });
+    }
   }
 };
 </script>
